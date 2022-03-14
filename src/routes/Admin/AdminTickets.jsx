dcf48f927878a10cd1063aa50840ebd23e7b6fca
Daniel <danielderu3@gmail.com>
2022-03-14 16:29:31 +0200

    added styling for the tickets

--- src/routes/Admin/AdminTickets.jsx
+++ src/routes/Admin/AdminTickets.jsx
@@ -1,15 +1,19 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router'
+
 
 import { DispayTickets } from '../../styled/Admin/Tickets.styled'
 
 const AdminTickets = () => {
+
+  const navigate = useNavigate()
   const [tickets, setTickets] = useState()
 
   const getTickets = async () => {
     try {
       const requestTickets = await axios("http://localhost:8000/admin/gettickets", {withCredentials: true})
-      // if(!requestTickets.data.tickets) setTickets(null)
+      if(!requestTickets.data.tickets) setTickets(null)
       console.log(requestTickets.data)
       setTickets(requestTickets.data.tickets)
     } catch (err) {
@@ -17,17 +21,28 @@ const AdminTickets = () => {
     }
   }
 
+  // const respondTicket = () => {
+
+  // }
+
   useEffect(async () => {
     await getTickets()
   }, [])
   return (
     <DispayTickets>
       <main>
+        <div className='headers'>
+          <div>No</div>
+          <div>Name</div>
+          <div>Email</div>
+          <div>Subject</div>
+        </div>
         <ul>
           {tickets && tickets.map((ticket, index) => (
-            <li key={ticket.id}>
+            <li key={ticket.id} onClick={() => null}>
               <div>{index+1}</div>
               <div>{ticket.first_name} {ticket.last_name}</div>
+              <div>{ticket.email}</div>
               <div>{ticket.subject}</div>
             </li>
           ))}
--- src/styled/Admin/Tickets.styled.js
+++ src/styled/Admin/Tickets.styled.js
@@ -1,16 +1,33 @@
 import styled from "styled-components"
 
 export const DispayTickets = styled.div`
+    font-size: 1.5em;
     main {
+        width: 70%;
+        margin: auto;
         ul {
-
+            padding: 0;
             li {
-                display: flex;
-
+                display: grid;
+                padding: 10px;
+                grid-template-columns: 1fr 3fr 3fr 6fr;
+                border: 2px solid transparent;
+                cursor: pointer;
                 &:nth-child(odd){
-                    background-color: grey;
+                    background-color: ${({theme}) => theme.colors.green};
+                }
+
+                &:hover {
+                    border: 2px solid ${({theme}) => theme.colors.lightBlue};
                 }
             }
+        }
+
+        .headers {
+            display: grid;
+            grid-template-columns: 1fr 3fr 3fr 6fr;
+            padding: 10px;
+            background-color: ${({theme}) => theme.colors.orange};
         }
     }
 
